80b26d38f3416adb4c2c688e958d4b8b88284bdf
Anish Kumar <anishk3843@gmail.com>
2025-11-09 15:29:33 +0530

    Enable split per ABI for smaller APK sizes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,6 +30,15 @@ android {
         versionName = flutter.versionName
     }
 
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86_64")
+            isUniversalApk = false
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
